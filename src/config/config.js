75468974e7cc3ed6756d14237e28adad93795b99
Suyash Mishra <55022525+SuyashMishra-dev@users.noreply.github.com>
2023-07-17 22:38:01 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,6 @@
-export const GET_MOVIE_URL = `http://www.omdbapi.com/`;
+// uncomment below line while running on localhost
+// export const GET_MOVIE_URL = `http://www.omdbapi.com/`;
+
+// comment the below line while running on localhost
+export const GET_MOVIE_URL = `https://www.omdbapi.com/`;
 export const API_KEY = '36eb55a';
